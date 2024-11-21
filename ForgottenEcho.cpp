e05069d1b348223f802e3adff8508dd430426b86
Orest Chumak <orest.chumak@gmail.com>
2024-11-22 00:28:37 +0200

    Add functions for drawing triangles and circles.

--- ForgottenEcho.cpp
+++ ForgottenEcho.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <stdlib.h>
+#include <cmath>
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 #include <GL/gl.h>
 // #include <glad/glad.h>
 
 using namespace std;
+
+//<><><> NEEDY CONSTANTS
+const float PiDiv180 = 3.1415f / 180.0f;
 
 //<><><> FUNCTIONS <><><>
 //call actions if key pressed
@@ -15,13 +19,41 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
         glfwSetWindowShouldClose(window, true);
     }
 }
+//coef for x coordinate
+float coefX(float x){
+    return (x / 1.5);
+}
+
+//draw a 2d circle
+void createCircle(float x, float y, float radius){ //x and y means centre of circle
+    // glColor3f(1.0f, 1.0f, 1.0f); //white color
+    glBegin(GL_TRIANGLE_FAN);
+    for(int i = 0; i < 360; i++){
+        x = radius * cos(i * PiDiv180);
+        y = radius * sin(i * PiDiv180);
+        glColor3f(x, y, 1.0f);
+        glVertex2f(coefX(x), y);
+    }
+    glEnd();
+}
+//draw simple 2d triangle
+void createTriangle(){
+    glBegin(GL_TRIANGLES);
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex2f(-0.5f, -0.5f);
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glVertex2f(0.0f, 0.5f);
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glVertex2f(0.5f, -0.5f);
+    glEnd();
+}
 
 //<><><><><> MAIN PROGRAM <><><><><>
 int main(void)
 {
     //check does glfw run or not
     if (!glfwInit()){
-        cout << "error: glfw can't run" << endl;
+        cout << "error: glfw didn'd run" << endl;
         exit(EXIT_FAILURE);
     }
 
@@ -54,16 +86,9 @@ int main(void)
     //loop
     while(!glfwWindowShouldClose(basicWindow)){
         glClear(GL_COLOR_BUFFER_BIT);
-        
-        //created triangle
-        glBegin(GL_TRIANGLES);
-        glColor3f(1.0f, 0.0f, 0.0f);
-        glVertex2f(-0.5, -0.5);
-        glColor3f(0.0f, 1.0f, 0.0f);
-        glVertex2f(0, 0.5);
-        glColor3f(0.0f, 0.0f, 1.0f);
-        glVertex2f(0.5, -0.5);
-        glEnd();
+
+        // createCircle(0.0f, 0.0f, 1.0f);
+        // createTriangle();
 
         //other needy actions
         glfwSwapBuffers(basicWindow);
