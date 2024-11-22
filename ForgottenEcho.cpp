afdbdfd20c079bbc7a2254a0ad10e1de974dea30
Orest Chumak <orest.chumak@gmail.com>
2024-11-22 21:32:37 +0200

    Add drawing simple sphere.

--- ForgottenEcho.cpp
+++ ForgottenEcho.cpp
@@ -12,32 +12,38 @@ using namespace std;
 const float PiDiv180 = 3.1415f / 180.0f;
 
 //<><><> FUNCTIONS <><><>
-//call actions if key pressed
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods){
-    if (key == 256 && action == 1){
+// call actions if key pressed
+static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
+{
+    if (key == 256 && action == 1)
+    {
         cout << "logs: window was closed" << endl;
         glfwSetWindowShouldClose(window, true);
     }
 }
-//coef for x coordinate
-float coefX(float x){
+// coef for x coordinate
+float coefX(float x)
+{
     return (x / 1.5);
 }
 
-//draw a 2d circle
-void createCircle(float x, float y, float radius){ //x and y means centre of circle
+// draw a 2d circle
+void createCircle(float x, float y, float radius)
+{ // x and y means centre of circle
     // glColor3f(1.0f, 1.0f, 1.0f); //white color
     glBegin(GL_TRIANGLE_FAN);
-    for(int i = 0; i < 360; i++){
+    for (int i = 0; i < 360; i++)
+    {
         x = radius * cos(i * PiDiv180);
         y = radius * sin(i * PiDiv180);
         glColor3f(x, y, 1.0f);
-        glVertex2f(coefX(x), y);
+        glVertex2f(x, y);
     }
     glEnd();
 }
-//draw simple 2d triangle
-void createTriangle(){
+// draw simple 2d triangle
+void createTriangle()
+{
     glBegin(GL_TRIANGLES);
     glColor3f(1.0f, 0.0f, 0.0f);
     glVertex2f(-0.5f, -0.5f);
@@ -48,61 +54,94 @@ void createTriangle(){
     glEnd();
 }
 
+void sphereCap(int section)
+{
+    //there must be code creating sphere caps using GL_TRIANGLE_FAN
+}
+
+//!!! ПОТРІБНО ДОРОБИТИ !!!
+void createSphere(int section)
+{
+    for (int stack = 0; stack < section; stack++)
+    { // sections up to down
+        glBegin(GL_QUAD_STRIP);
+
+        for (int sector = 0; sector <= section; sector++)
+        { // sections around
+            glColor3f(0.1f * sector, 0.1f * sector, 0.1f * sector);
+
+            float sec = 6.28f * (float)sector / (float)section;
+            float stk = 1.57f - 3.14f * (float)stack / (float)section;
+
+            glVertex3f(cos(stk) * cos(sec), cos(stk) * sin(sec), sin(stk));
+            stk = 1.57f - 3.14f * (float)(stack + 1) / (float)section;
+            glVertex3f( cos(stk) * cos(sec), cos(stk) * sin(sec), sin(stk));
+
+        }
+        glEnd();
+    }
+}
+
 //<><><><><> MAIN PROGRAM <><><><><>
 int main(void)
 {
-    //check does glfw run or not
-    if (!glfwInit()){
+    // check does glfw run or not
+    if (!glfwInit())
+    {
         cout << "error: glfw didn'd run" << endl;
         exit(EXIT_FAILURE);
     }
 
-    //time and fps
+    // time and fps
     double time = glfwGetTime();
     glfwSwapInterval(1);
 
-    //create window
-    GLFWwindow* basicWindow = glfwCreateWindow(600, 400, "Basic Window", NULL, NULL);
-    if (!basicWindow){
+    int width{600};
+    int height{400};
+
+    // create window
+    GLFWwindow *basicWindow = glfwCreateWindow(width, height, "Basic Window", NULL, NULL);
+    if (!basicWindow)
+    {
         cout << "error: window can't be created" << endl;
         exit(EXIT_FAILURE);
     }
     glfwMakeContextCurrent(basicWindow);
 
-    //key enter function
+    // key enter function
     glfwSetKeyCallback(basicWindow, key_callback);
 
-    //size of window
-    int width {600};
-    int height {400};
+    // size of window
     glfwGetFramebufferSize(basicWindow, &width, &height);
     glViewport(0, 0, width, height);
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
-    //set coordinates system for window
+    // set coordinates system for window
     glLoadIdentity();
-    glOrtho(-1, 1, -1, 1, -1, 1);
+    glOrtho(-((float)width / (float)height), ((float)width / (float)height), -1, 1, -1, 1);
 
-    //loop
+    // loop
     float angle = 0.0f;
-    while(!glfwWindowShouldClose(basicWindow)){
+    while (!glfwWindowShouldClose(basicWindow))
+    {
         glClear(GL_COLOR_BUFFER_BIT);
 
         // createCircle(0.0f, 0.0f, 1.0f);
-        createTriangle();
+        // createTriangle();
+        createSphere(15);
 
-        //rotate object and change that position
-        glTranslated(cos(angle) / 100.0f, 0.0f, 0.0f);
-        glRotated(2.0f, 1.0f, 0.0f, 0.0f);
+        // rotate object and change that position
+        //  glTranslated(cos(angle) / 100.0f, 0.0f, 0.0f);
+        //  angle += 0.01;
+        glRotated(1.0f, 1.0f, 1.0f, 1.0f);
+        
 
-        //other needy actions
+        // other needy actions
         glfwSwapBuffers(basicWindow);
         glfwPollEvents();
-
-        angle += 0.01;
     }
 
-    //close everything
+    // close everything
     glfwDestroyWindow(basicWindow);
     glfwTerminate();
 }
