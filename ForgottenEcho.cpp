b500ffa4f0165f8b4299fff74fa22603a875351d
Orest Chumak <orest.chumak@gmail.com>
2024-11-27 22:13:08 +0200

    Add gluLookAt and gluPerspective.

--- ForgottenEcho.cpp
+++ ForgottenEcho.cpp
@@ -5,12 +5,16 @@
 #include <GLFW/glfw3.h>
 #include <GL/gl.h>
 #include <SOIL/SOIL.h>
+#include <GL/glut.h>
 // #include <glad/glad.h>
 
 using namespace std;
 
 //<><><> NEEDY CONSTANTS
 const float PiDiv180 = 3.1415f / 180.0f;
+int width{600};
+int height{400};
+
 int textureID;
 int sphere;
 
@@ -102,6 +106,8 @@ void createSphere(int section)
     sphereCap(section, -1);
 }
 
+
+//[4] functions for creation triangle sphere
 void normalize(float vec[]){
     float coef = 1.0f / (sqrt(pow(vec[0], 2) + pow(vec[1], 2) + pow(vec[2], 2)));
     vec[0] *= coef;
@@ -114,8 +120,8 @@ void vertex(float a[]){
     float y {a[1]};
     float z {a[2]};
 
-    float phi = 1.0f - (1.0f + atan2(x, -z) / 3.14f) / 2.0f;
-    float theta = acos(y) / 3.14f;
+    float phi = 1.0f - (1.0f + atan2(x, -z) / 3.1415f) / 2.0f;
+    float theta = acos(y) / 3.1415f;
 
     glTexCoord2f(phi, theta);
     glVertex3fv(a);
@@ -138,39 +144,35 @@ void multiTriangle(float a[], float b[], float c[], int resolution){
     else{
         glBindTexture(GL_TEXTURE_2D, textureID);
         glBegin(GL_TRIANGLES);
-        float color = 1.0f; //fabs(1.0f * a[0]) + 0.1f;
-        glColor3d(color, color, color);
+        glColor3d(1.0f, 1.0f, 1.0f);
         vertex(a);
         vertex(b);
         vertex(c);
-        
-        // glVertex3fv(b);
-        // glVertex3fv(c);
         glEnd();
     }
 }
 
 void triangleSphere(int resolution){ //right now resolution = 1
 
-    float angle60 = 6.28f / 3.0f;
+    float angle60 = 6.283f / 3.0f;
     for(int i = 0; i < 3; i++){
         
         float a[3] {0.0f, cos(0.0f), 0.0f};
         float b[3] {cos(angle60 * i) * sin(angle60), cos(angle60), sin(angle60 * i) * sin(angle60)};
-        float c[3] {cos(angle60 * (i + 1)) * sin(angle60), cos(angle60), sin(angle60 * (i + 1)) * sin(angle60)};
+        float c[3] {cos(angle60 * (i + 1.0f)) * sin(angle60), cos(angle60), sin(angle60 * (i + 1.0f)) * sin(angle60)};
         
         multiTriangle(a, b, c, resolution);
     }
     float a[3] {sin(angle60), cos(angle60), 0.0f};
     float b[3] {cos(angle60) * sin(angle60), cos(angle60), sin(angle60) * sin(angle60)};
-    float c[3] {cos(angle60 * 2) * sin(angle60), cos(angle60), sin(angle60 * 2) * sin(angle60)};
+    float c[3] {cos(angle60 * 2.0f) * sin(angle60), cos(angle60), sin(angle60 * 2.0f) * sin(angle60)};
     multiTriangle(a, b, c, resolution);
 }
 
 //<><><><><> MAIN PROGRAM <><><><><>
 int main(void)
 {
-    // check does glfw run or not
+    // check did glfw run or not
     if (!glfwInit())
     {
         cout << "error: glfw didn'd run" << endl;
@@ -181,9 +183,6 @@ int main(void)
     double time = glfwGetTime();
     glfwSwapInterval(1);
 
-    int width{600};
-    int height{400};
-
     // create window
     GLFWwindow *basicWindow = glfwCreateWindow(width, height, "Basic Window", NULL, NULL);
     if (!basicWindow)
@@ -192,24 +191,35 @@ int main(void)
         exit(EXIT_FAILURE);
     }
     glfwMakeContextCurrent(basicWindow);
-    textureID = SOIL_load_OGL_texture("./solarsystemscope/2k_moon.jpg", SOIL_LOAD_RGB, SOIL_CREATE_NEW_ID, 0);
-    cout << "texture: " << textureID << endl;
-
-    //enable gl functions
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_TEXTURE_2D);
-
-    // key enter function
-    glfwSetKeyCallback(basicWindow, key_callback);
-
     // size of window
     glfwGetFramebufferSize(basicWindow, &width, &height);
     glViewport(0, 0, width, height);
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-
     // set coordinates system for window
     glLoadIdentity();
     glOrtho(-((float)width / (float)height), ((float)width / (float)height), -1, 1, -1, 1);
+
+
+    //add texture
+    textureID = SOIL_load_OGL_texture("./solarsystemscope/2k_moon.jpg", SOIL_LOAD_RGB, SOIL_CREATE_NEW_ID, 0);
+    cout << "texture: " << textureID << endl;
+
+
+    //enable gl functions
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_TEXTURE_2D);
+    // key enter function
+    glfwSetKeyCallback(basicWindow, key_callback);
+
+
+
+    gluLookAt(0.0f, 0.0f, 1.0f,
+              0.0f, 0.0f, 0.0f,
+              0.0f, 1.0f, 0.0f);
+    gluPerspective(90.0f, width/height, -0.1f, -3.0f);
+    // glPushMatrix();
+    // glPopMatrix();
+
 
     // loop
     float angle = 0.0f;
@@ -223,16 +233,16 @@ int main(void)
         if (sphere == 0){
             sphere = glGenLists(1);
             glNewList(sphere, GL_COMPILE);
-            triangleSphere(4);
+            triangleSphere(5);
             glEndList();
         }
         glCallList(sphere);
 
         // rotate object and change that position
-        //  glTranslated(cos(angle) / 100.0f, 0.0f, 0.0f);
-        //  angle += 0.01;
-        glRotated(1.0f, 1.0f, 1.0f, 0.0f);
-        
+        // glLoadIdentity();
+        // glTranslated(0.0f, 0.0f, -0.008f);
+        // angle += 0.01;
+        glRotated(0.2f, 0.0f, 1.0f, 0.0f);
 
         // other needy actions
         glfwSwapBuffers(basicWindow);
